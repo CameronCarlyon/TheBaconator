941724729eccc4bb06a67d73848dd95ecb971418
RikRogaar <rikrogaar@gmail.com>
2023-09-19 22:45:42 +0200

    fix logic

---
+++ commands.js
@@ -0,0 +1,7 @@
+const commands = [
+  { key: "rikkie", value: "retarded doom player" },
+  { key: "hello there", value: "obi juan" },
+  { key: "bacon", "value": "you make me crazy bbg :heart:"}
+];
+
+module.exports = commands;
