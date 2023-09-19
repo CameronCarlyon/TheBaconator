4d29c786135dadfed5f82c37e3360e02b76ef7c1
Cameron Carlyon <cameroncarlyon@gmail.com>
2023-09-19 20:31:46 +0100

    Added Commands

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+console.log("initialising...");
 require("dotenv").config();
 
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
 
-client.once(Events.ClientReady, () => {
-  console.log("The Baconator is ready to roll!");
+client.once(Events.ClientReady, (c) => {
+  console.log(`The ${c.user.tag} is ready to roll!`);
+});
+
+client.on("messageCreate", (message) => {
+  console.log(message);
+  if (message.content === "Hello there!") {
+    message.reply("General Kenobi!");
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
