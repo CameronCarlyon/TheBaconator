1362799248e3ff9dbf33e0ee65b251896267543f
Cameron Carlyon <cameroncarlyon@gmail.com>
2023-09-21 02:07:51 +0100

    Update keywords.js

--- commands/keywords.js
+++ commands/keywords.js
@@ -3,6 +3,8 @@ const keywords = [
   { keyword: "hello there", reply: "General Kenobi!" },
   { keyword: "rik", reply: "DOOMFIST IS BAD 😭" },
   { keyword: "chisy", reply: "Living in Spain with and without the S." },
+  { keyword: "petit", reply: "Fre*ch 🤢"},
+  { keyword: "lucas", reply: "Fre*ch 🤢"},
 ];
 
 module.exports = keywords;
